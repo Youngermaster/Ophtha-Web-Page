d0c259ef5224b8b00a2a8625e395d58915d2139f
Youngermaster <jmyoungh@eafit.edu.co>
2019-04-22 06:43:16 -0500

    I have changed the folder paths

--- src/js/menu.js
+++
@@ -1,7 +0,0 @@
-let mennuButton = document.getElementById("menu-button")
-let menu = document.getElementById("menu")
-
-mennuButton.addEventListener("click", function() {
-    'use strict';
-    menu.classList.toggle('show-menu')
-})
